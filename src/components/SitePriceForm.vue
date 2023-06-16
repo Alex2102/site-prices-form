dddf8b0bbdbbbfc5b08129e185f68c89026e0aaa
Alex <aap@solar.bi>
2023-06-16 18:06:02 +0500

    не большие правки

--- src/components/SitePriceForm.vue
+++ src/components/SitePriceForm.vue
@@ -1,42 +1,48 @@
 <template>
-  <v-card max-width="800" class=price__edit-card" color="background" variant="flat">
+  <v-card max-width="800" class="price__edit-card" classNamer="background" variant="flat">
     <v-card-item>
       <v-form ref="form">
         <v-row class=mt-0>
-          <v-col cols="12">
-            <v-text-field
-                v-model="title"
-                variant="outlined"
-                label="Название"
-                :rules="requiredValidation"
-                clearable
-            />
+          <vclassNamecols
+          ="12">
+          <v-text-field
+              v-model="title"
+              variant="outlined"
+              label="Название"
+              :rules="requiredValidation"
+              clearable
+          />
           </v-col>
         </v-row>
 
         <v-row class="mt-0">
-          <v-col cols="12">
-            <div class="date__field-wrap">
-              <div class="left-block">
-                <s-date-picker
-                    v-model="startDate"
-                    :max-date="endDate"
-                    label="Дата начала действия прайса"/>
-              </div>
-              <hr>
-              <div class="right-block">
-                <s-date-picker
-                    v-model="endDate"
-                    :min-date="startDate"
-                    label="Дата окончания действия прайса"/>
-              </div>
-            </div>
+          classNamel cols="12">
+          <div class="date__field-wrap" className
+          <div class="left-block">
+            className
+            <s-date-picker
+                v-model="startDate"
+                :max-date="endDate"
+                :in-valid="!startDateValid"
+                label="Дата начала действия прайса"/>
+          </div>
+          <hr>
+          <div class="right-block">
+            className
+            <s-date-picker
+                v-model="endDate"
+                :min-date="startDate"
+                :in-valid="!endDateValid"
+                label="Дата окончания действия прайса"/>
+          </div>
+          </div>
           </v-col>
         </v-row>
 
         <v-row class=mt-0>
-          <v-col cols="12">
-            <slider-footer-btns :show-apply="false" @save="save" @cancel="closeSlider"/>
+          <vclassNamecols
+          ="12">
+          <slider-footer-btns :show-apply="false" @save="save" @cancel="closeSlider"/>
           </v-col>
         </v-row>
       </v-form>
@@ -47,11 +53,12 @@
 <script>
 import SliderFooterBtns from "@/../../../../../../../modules/solar.booking/lib/ui/controls/SliderFooterBtns.vue"
 import SDatePicker from "@/../../../../../../../modules/solar.booking/lib/ui/fields/SDatePicker.vue"
+import {runComponentAction} from "@/../../../../../../../modules/solar.booking/lib/api"
 
 export default {
   name: 'site-price-form',
   components: {SliderFooterBtns, SDatePicker},
-  data:() =>({
+  data: () => ({
     entityId: null,
     siteId: null,
 
@@ -59,7 +66,9 @@ export default {
     startDate: null,
     endDate: null,
 
-    requiredValidation: [v=>!!v || 'Поле обязательно для заполнения']
+    requiredValidation: [v => !!v || 'Поле обязательно для заполнения'],
+    startDateValid: true,
+    endDateValid: true
   }),
 
   async mounted() {
@@ -81,9 +90,30 @@ export default {
     async save(closeSlider = 'Y') {
       const {valid} = await this.$refs.form.validate()
 
-      if(!valid) return
+      if (this.startDate === null) this.startDateValid = false
+      if (this.endDate === null) this.endDateValid = false
 
+      if (!valid || this.startDateValid !== true || this.endDateValid !== true) return
 
+      let formData = new FormData()
+
+      formData.set('siteId', this.siteId)
+      formData.set('id', this.entityId)
+      formData.set('title', this.title)
+      formData.set('startDate', this.getFormattedDate(this.startDate))
+      formData.set('endDate', this.getFormattedDate(this.endDate))
+
+      const result = await runComponentAction('solar:booking.sites.price.edit', 'save', formData)
+
+      console.log(result)
+    },
+    getFormattedDate(date) {
+      let monthTemp = +date.getMonth() + 1
+
+      const day = +date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
+      const month = monthTemp < 10 ? `0${monthTemp}` : monthTemp
+
+      return `${day}.${month}.${date.getFullYear()}`
     },
     closeSlider() {
       closeCurrentSlider()
@@ -97,6 +127,7 @@ export default {
 .price__edit-card .v-card-item__content {
   overflow: inherit !important;
 }
+
 .date__field-wrap {
   display: flex;
   justify-content: space-between;
